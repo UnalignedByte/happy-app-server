850c4a9a9df3504aa0c5b2bf83fb12317b8fe442
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-04-28 22:53:12 +0100

    Added command line parsting to start in normal or fcgi mode

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 import "os"
 import "net/http"
-import "github.com/gorilla/mux"
+//import "github.com/gorilla/mux"
+import "flag"
+import "strconv"
+import "errors"
+
+var isUsingFastCGI = flag.Bool("fcgi", false, "Should run in FastCGI mode?")
+var nonFastCGIPort = flag.Int("no-fcgi", 80, "TCP port to use in non FastCGI mode")
 
 type Route struct {
     Path string
@@ -15,13 +21,40 @@ var routes = []Route {
 }
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
+    flag.Parse()
+
+    if !*isUsingFastCGI && len(os.Args) > 1 {
+        port, err := strconv.Atoi(os.Args[1])
+        if err == nil {
+            *nonFastCGIPort = port
+        }
+    }
+
+    if !*isUsingFastCGI && *nonFastCGIPort < 1 {
+        checkError(errors.New("Invalid port number"))
+    }
+
+    if *isUsingFastCGI {
+        startFastCGI()
+    } else {
+        start(*nonFastCGIPort)
+    }
+}
+
+func start(port int) {
+    fmt.Printf("Starting normal mode @ %d\n", port)
+
+    /*router := mux.NewRouter().StrictSlash(true)
     for _, route := range routes {
         router.Path(route.Path).Handler(route.Handler)
     }
 
     err := http.ListenAndServe(":8080", router)
-    checkError(err)
+    checkError(err)*/
+}
+
+func startFastCGI() {
+    fmt.Println("Starting FastCGI mode")
 }
 
 func checkError(err error) {
